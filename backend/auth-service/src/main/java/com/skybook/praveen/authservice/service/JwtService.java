e99dadb123dc226609c4009a160a05497142d444
praveencloudlab <praveen.somireddy@gmail.com>
2026-06-29 02:21:14 +0100

    feat(auth): add JWT validation methods

--- backend/auth-service/src/main/java/com/skybook/praveen/authservice/service/JwtService.java
+++ backend/auth-service/src/main/java/com/skybook/praveen/authservice/service/JwtService.java
@@ -1,37 +1,60 @@
 package com.skybook.praveen.authservice.service;
-
+import com.skybook.praveen.authservice.config.JwtProperties;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import com.skybook.praveen.authservice.config.JwtProperties;
+
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
-@RequiredArgsConstructor
 public class JwtService {
 
     private final JwtProperties jwtProperties;
 
-    public String generateToken(String email) {
+    public JwtService(JwtProperties jwtProperties) {
+        this.jwtProperties = jwtProperties;
+    }
 
+    public String generateToken(String email) {
         Date now = new Date();
         Date expiryDate = new Date(now.getTime() + jwtProperties.getExpiration());
-        System.out.println(">>>>>>>>>>>>>>>>>>>>"+jwtProperties.getExpiration()+">>>>>>>>>>>>>>>>>>>>>>>>");
 
         return Jwts.builder()
                 .subject(email)
                 .issuedAt(now)
                 .expiration(expiryDate)
-                .signWith(getSecretKey())
+                .signWith(getSigningKey())
                 .compact();
     }
 
-    private SecretKey getSecretKey() {
-        return Keys.hmacShaKeyFor(
-                jwtProperties.getSecret().getBytes(StandardCharsets.UTF_8)
-        );
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token, String email) {
+        String username = extractUsername(token);
+        return username.equals(email) && !isTokenExpired(token);
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractAllClaims(token)
+                .getExpiration()
+                .before(new Date());
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = jwtProperties.getSecret().getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
